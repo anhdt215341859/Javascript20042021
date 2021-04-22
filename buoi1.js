9512c1a3a6c9ec15daa29c9b76856dbb377697e5
do tien anh <anhdt.ute@gmail.com>
2021-04-22 18:29:32 +0700

    thay doi file buoi 1

--- buoi1.js
+++ buoi1.js
@@ -2,5 +2,10 @@
 // git init tạo thư mục quản lí soure code
 // 2:kiểm tra sự thay đổi của folder (git status)
 //git status
-// lưu trữ thay đổi: git add tenfile (lưu 1 file)
+// 3lưu trữ thay đổi: git add tenfile (lưu 1 file)
 // git add .(lưu tất cả sự thay đổi)
+// 4: commit vào mục repository
+// git commit -m "lưu file buổi 1"
+/*git config --global user.name "do tien anh"
+git config --global user.email "anhdt.ute@gmail.com" */
+
