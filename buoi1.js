047d57f08a52ebe8c2840d4ee0c3ac5bad6c32fe
do tien anh <anhdt.ute@gmail.com>
2021-04-22 18:15:35 +0700

    add file buoi1

---
+++ buoi1.js
@@ -0,0 +1,6 @@
+//1: tạo thư mục quản lí soure code
+// git init tạo thư mục quản lí soure code
+// 2:kiểm tra sự thay đổi của folder (git status)
+//git status
+// lưu trữ thay đổi: git add tenfile (lưu 1 file)
+// git add .(lưu tất cả sự thay đổi)
