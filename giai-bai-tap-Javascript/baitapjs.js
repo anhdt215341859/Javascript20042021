1945d6dffc49c9dc709258b4898258e3bf09095f
do tien anh <anhdt.ute@gmail.com>
2021-04-25 10:25:37 +0700

    bài 1 khai báo biến

--- giai-bai-tap-Javascript/baitapjs.js
+++ giai-bai-tap-Javascript/baitapjs.js
@@ -1,1 +1,31 @@
 // bài 1
+// khai báo biến
+let x = 8
+let y = 4
+let a 
+let b 
+let c 
+let d 
+ a = x + y
+ b = x - y
+ c = x * y
+ d = x / y
+console.log(a,b,c,d)
+// function kiemtrasoNguyento(number) {
+//     let count= 0
+//     if(number<2){
+//         console.log("Khong phai la so nguyen to")
+//         return
+//     }
+
+//     for(let i=2;i <= number; i++){
+//         if (number%i ==0)
+//         count++;
+//     }
+//     if (count ==1){
+//         console.log("la so nguyen to")
+
+//     }
+//     else {console.log("không la so nguyen to")}
+// }
+// kiemtrasoNguyento(0)
