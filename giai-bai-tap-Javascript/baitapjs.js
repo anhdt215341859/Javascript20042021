c1c32bdf1d3f460aa4cb02cb0fdc13c127d27e75
do tien anh <anhdt.ute@gmail.com>
2021-04-25 11:29:51 +0700

    khai báo biến và object

--- giai-bai-tap-Javascript/baitapjs.js
+++ giai-bai-tap-Javascript/baitapjs.js
@@ -1,16 +1,27 @@
 // bài 1
 // khai báo biến
-let x = 8
-let y = 4
-let a 
-let b 
-let c 
-let d 
- a = x + y
- b = x - y
- c = x * y
- d = x / y
-console.log(a,b,c,d)
+// let x = 8
+// let y = 4
+// let a 
+// let b 
+// let c 
+// let d 
+//  a = x + y
+//  b = x - y
+//  c = x * y
+//  d = x / y
+// console.log(a,b,c,d)
+
+// bài 2 - kiểu dữ liệu
+var name = 'Đỗ Tiến Anh'
+var age = 25
+var manymoney = false
+var thoitiet = {
+    temp = 30, 
+    rain = false,
+    location = 'tan binh, sai gon, viet nam',
+}
+console.log(thoitiet.location)
 // function kiemtrasoNguyento(number) {
 //     let count= 0
 //     if(number<2){
