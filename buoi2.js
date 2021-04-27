cc5bf55e74e5585642a61d7908a12a1d8a555860
do tien anh <anhdt.ute@gmail.com>
2021-04-27 19:20:38 +0700

    hàm map

--- buoi2.js
+++ buoi2.js
@@ -160,3 +160,25 @@
     console.log(output);
     // Expect: [4, 8, 12]
     
+    // 15 – Hàm map, 
+    //thay đổi các giá trị phần tử trong mảng, k thay đổi kích thước trong mảng
+    // khi sự thay đổi đc thực hiện, sẽ tạo ra mảng mới chứa mảng thay đổi
+//Ví dụ
+
+const arrNumbers = [1, 2, 3, 4, 5]
+
+const newArrNumbers = arrNumbers.map(function(number, index) {
+    console.log(number)
+    return number
+})
+consloe.log(newArrNumbers)
+/**
+ * 1. Viết hàm tripple nhận vào 1 số và trả về số đó nhân 3
+ * 2. Sử dụng map method và hàm tripple để nhân 3 các phần tử trong mảng dưới đây
+ */
+// Khai báo tripple function ở đây
+
+
+
+// var numbers = [10, 5, 8];
+// Gợi ý: numbers.map... (tự viết tiếpe
