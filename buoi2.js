1cec47f5393ad22d7a7646ef2db2895cd0f3380c
do tien anh <anhdt.ute@gmail.com>
2021-04-27 18:27:17 +0700

    bài 12 - for in

--- buoi2.js
+++ buoi2.js
@@ -9,20 +9,83 @@
  * Sử dụng vòng lặp for...of để tính tích của các phần tử trong một array gồm các số
  */
 
-    function multiply(arr){ 
-    if(Array.isArray(arr))
-    {
-        let mod = 1;
-        for( let x of arr ) 
-        {
-            mod *=x;
-        }
-        return mod;
-    }
+//     function multiply(arr){ 
+//     if(Array.isArray(arr))
+//     {
+//         let mod = 1;
+//         for( let x of arr ) 
+//         {
+//             mod *=x;
+//         }
+//         return mod;
+//     }
 
-else
-{
-    return "giá trị không phải mảng"
-}
+// else
+// {
+//     return "giá trị không phải mảng"
+// }
+//     }
+// console.log(multiply([2, 3, 4])); // expect: 24
+
+// for in
+//  const arrNames = ["ti","suu", "dan"]
+//  const teo= {
+//      name="teo",
+//      age= 24
+//  }
+
+//  for (const key in teo) {
+//      console.log(key)
+//      }
+    
+
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * Gợi ý: đây là bài tập khó, bạn nên tìm hiểu về recursive trước
+ */
+
+ var apartment = {
+    bedroom: {
+      area: 20,
+      bed: {
+        type: 'twin-bed',
+        price: 100
+      }
     }
-console.log(multiply([2, 3, 4])); // expect: 24
+  };
+  
+  function getkey(object){
+      for(const key in object){
+          console.log(key);
+          for (const keyBedroom in object[key]) {
+              console.log(keyBedroom)
+              for (const keyBed in object[key][keyBedRoom]){
+                  console.log(keyBed)
+              }
+                   
+              }
+          }
+      }
+  
+  
+  getkey(apartment)
+  
+  /**
+   * Kết quả mong muốn:
+   * bedroom
+   * area
+   * bed
+   * type
+   * price
+   * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+   */
+       
+ function getkey(object){
+     for (const key in object) {
+         console.log(key)
+         if (typeof object[key]=== 'object') {
+            getkey(object[key])
+             
+         }
+     }
+ } 
