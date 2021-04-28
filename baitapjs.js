94216dea14c1d85fcc4d7e941c9ee3c0cca231d5
do tien anh <anhdt.ute@gmail.com>
2021-04-28 15:08:24 +0700

    vòng lặp for in, xuất ra tất cả phần tử có trng mảng

--- baitapjs.js
+++ baitapjs.js
@@ -264,13 +264,78 @@
 // console.log(multiply([2, 3, 4])); // expect: 24
 
 // for in
- const arrNames = ["ti", "suu", "dan"]
- let meo = {
-    name : 'do tien anh',
-    age: 24
- }
+//  const arrNames = ["ti", "suu", "dan"]
+//  let meo = {
+//     name : 'do tien anh',
+//     age: 24
+//  }
 
- for (let key in meo) {
-     console.log(key);
-     }
-
+//  for (let key in meo) {
+//      console.log(key);
+//      }
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * Gợi ý: đây là bài tập khó, bạn nên tìm hiểu về recursive trước
+ */
+ var apartment = {
+    bedroom: 
+    {
+      area: 20,
+      bed: 
+        {
+         type: 'twin-bed',
+         price: 100
+        }
+    }
+  };
+  function getkey(object){
+      for (const key in object) {
+          console.log(key);
+          for (const keybedroom in object[key]) {
+              console.log(keybedroom);
+              for (const keybed in object[key][keybedroom]) {
+                  console.log(keybed);
+                  }
+              }
+                  
+        }
+    }
+    getkey(apartment)
+//   function getkey(object){
+//       for (const keymain in object) {
+//           console.log(keymain) //keymain: bedroom
+//           for(const keybedroom in object[keymain]){
+//               console.log(keybedroom) // key_bedroom: area , bed
+//           }
+//           for (const keybed in object[keymain][keybedroom])
+//           {
+//             console.log(keybed)
+//         }
+//           }
+//       }
+//     getkey(apartment)
+   /**
+   * Kết quả mong muốn:
+   * bedroom
+   * area
+   * bed
+   * type
+   * price
+   * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+   */
+//     function getkey(object){
+//       for(const key in object){
+//           console.log(key);
+//           for (const keyBedroom in object[key]) {
+//               console.log(keyBedroom)
+//               for (const keyBed in object[key][keyBedroom]){
+//                   console.log(keyBed)
+//               }
+                   
+//               }
+//           }
+//       }
+  
+  
+//   getkey(apartment)
+  
